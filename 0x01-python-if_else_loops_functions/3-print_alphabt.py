c9e9866edd204e604d89ffc189b85d3601728a7e
samiunique <yitesam@gmail.com>
2023-11-01 14:17:54 -0700

    exclude e&q

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -2,4 +2,5 @@
 """Print the alphabet in lowercase, not followed by a new line."""
 
 for letter in range(97, 123):
-    print("{}".format(chr(letter)), end="")
+    if chr(letter) not in ['e', 'q']:
+        print("{}".format(chr(letter)), end="")
