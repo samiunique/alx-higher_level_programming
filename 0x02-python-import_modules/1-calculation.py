459e88a133193a589592205d017255e2b24223bc
samiunique <yitesam@gmail.com>
2023-11-04 11:54:06 -0700

    comment

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    """Print the sum of 1 and 2."""
+    """Print the sum, difference, multiple and quotient of 10 and 5."""
     from calculator_1 import add
     from calculator_1 import sub
     from calculator_1 import mul
