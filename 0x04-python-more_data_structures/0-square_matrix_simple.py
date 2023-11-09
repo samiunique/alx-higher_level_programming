1004cf46918a8a4874e1793f357a9d938a892162
samiunique <yitesam@gmail.com>
2023-11-09 05:48:18 -0800

    remove blank lines for pycodestyle

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -6,6 +6,4 @@ def square_matrix_simple(matrix=[]):
     """
     # Use list comprehension to square each element in each column
     new_matrix = [[x**2 for x in col] for col in matrix]
-    
     return new_matrix
-
