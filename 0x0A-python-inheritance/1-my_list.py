6dac2eccdc9d29261396fe203a79c0e971be729d
samiunique <yitesam@gmail.com>
2023-12-04 19:20:04 -0800

    move main files to main folder

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python3
+"""
+contains the MyList class
+"""
+
+
 class MyList(list):
+    """
+contains the MyList class sub
+"""
     def print_sorted(self):
+        """initializes the object"""
         sorted_list = sorted(self)
+        """prints the sorted list"""
         print(sorted_list)
