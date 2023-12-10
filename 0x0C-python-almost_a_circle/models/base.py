0aa4ae11b1ece073bcd6e65e1726033603652666
samiunique <yitesam@gmail.com>
2023-12-10 08:26:59 -0800

    json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,3 +1,7 @@
+import json
+import csv
+import turtle
+
 class Base:
     """ Private class attribute """
     __nb_objects = 0
@@ -20,3 +24,10 @@ class Base:
                 new_id = Base.__nb_objects
             self.id = new_id
             Base.used_ids.add(new_id)
+
+    def to_json_string(list_dictionaries):
+        """ Return the JSON serialization of a list of dicts. """
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
