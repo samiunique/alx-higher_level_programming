12ceba8336447ccf35060d3e16d38bf25151e2c1
samiunique <yitesam@gmail.com>
2023-12-09 22:00:59 -0800

    tasks, mains and 2, 3

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,22 @@
+class Base:
+    """ Private class attribute """
+    __nb_objects = 0
+    """ Set to store used IDs """
+    used_ids = set()
+
+    def __init__(self, id=None):
+        if id is not None:
+            if id in Base.used_ids:
+                raise ValueError("ID already in use")
+            self.id = id
+            """ Add the ID to used_ids """
+            Base.used_ids.add(id)
+        else:
+            Base.__nb_objects += 1
+            new_id = Base.__nb_objects
+            """ Find a non-used ID """
+            while new_id in Base.used_ids:
+                Base.__nb_objects += 1
+                new_id = Base.__nb_objects
+            self.id = new_id
+            Base.used_ids.add(new_id)
