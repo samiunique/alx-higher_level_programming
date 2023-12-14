f42cf7660505c8fc600becaf179bdb2de281e9ac
samiunique <yitesam@gmail.com>
2023-12-13 19:48:32 -0800

    test

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,20 +1,34 @@
+#!/usr/bin/python3
+
+"""Defines a base model class."""
 import json
 import csv
 import turtle
 
 
 class Base:
-    """ Private class attribute """
+    """Base model.
+
+    This Represents the "base" for all other classes in project 0x0C*.
+
+    Private Class Attributes:
+        __nb_object (int): Number of instantiated Bases.
+    """
+
     __nb_objects = 0
-    """ Set to store used IDs """
 
     def __init__(self, id=None):
+        """Initialize a new Base.
+
+        Args:
+            id (int): The identity of the new Base.
+        """
         if id is not None:
             self.id = id
-            """ Add the ID to used_ids """
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
     @staticmethod
     def to_json_string(list_dictionaries):
         """Return the JSON serialization of a list of dicts.
